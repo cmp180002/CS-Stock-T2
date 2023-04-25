5aeb85ac3b5069063a76559b12f27b23dc7aed41
Valentine <sav180001@utdallas.edu>
2023-04-25 11:14:26 -0500

    Renamed component files

--- src/gpt-api/Create_Finetunes.java
+++ src/gpt-api/Create_Finetunes.java
@@ -5,3 +5,7 @@
       This program processes the data produced by the web scraping component and converts
       the data into prompt-completion pairs of the correct format for fine-tuning.
 */
+
+public class Create_Finetunes {
+
+}
