e0feec9984943b8585dda2fc27a37db19584a393
Valentine <sav180001@utdallas.edu>
2023-04-28 17:00:21 -0500

    Basic framework for Main.java done.

--- src/gpt-api/Main.java
+++ src/gpt-api/Main.java
@@ -7,5 +7,14 @@
       actually train the model with the prompt-completion pairs.
 */
 public class Main {
+  public static void main(String[]args) {
+    // Create Fine-tuning Data for the Day
+    // finetuning_file is the filename of the created fine-tunes
+    String finetuning_file = Create_Finetunes.ProduceFinetuningData();
 
+    // Train the Model with the Fine-tunes
+    if (!Train_Model.TrainModel(finetuning_file)) {
+      System.out.println("Finetune Failed.");
+    }    
+  }
 }
